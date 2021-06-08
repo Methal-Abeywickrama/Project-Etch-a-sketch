103f50712dbd10dcf617f511162d276e4a999896
Methal Abeywickrama <methal8779@gmail.com>
2021-06-08 23:23:21 +0530

    Finalized v1

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const container = document.querySelector("#container");
+const submit = document.querySelector("#submit");
+const root = document.querySelector(":root");
+const totalWidth = window.screen.width;
+console.log (totalWidth);
 
-const numbergetter = () => {
-    let number = parseInt('Enter a number between 1 and 100');
-    if (number){}
-};
+const rootSetter = (num) => {
+    let boxwidth = 600 / num;
+    let boxwidthString = boxwidth + "px";
+    let leftMargin = totalWidth / 4;
+    console.log(window.screen.width)
+    console.log(leftMargin);
+    root.style.setProperty('--num', num);
+    root.style.setProperty('--length', boxwidthString);
+    root.style.setProperty('--leftMargin', leftMargin);
+}
 
-const create2grids = () => {
+
+
+const createFirstGrid = () => { 
     for (let i = 1; i <= 16 * 16; i++) {
         const box = document.createElement('div');
-        box.classList.add('box');
+
         container.appendChild(box);
+        box.classList.add('box');
         box.addEventListener('mouseover', addColor)
     }
 };
@@ -18,8 +31,43 @@ const addColor = (e) => {
 };
 
 
+const gridNumberGetter = () => {
+    let userInput = parseInt(prompt("enter a number between 1 and 100"));
+    if (userInput <= 1 || userInput >= 100) {
+        gridNumberGetter ();
+    };
+    return userInput;
 
-create2grids();
+};
+
+const boxRemover = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    };
+};
+
+const createNextGrid = (num) => {
+    for (let i = 1; i <= num * num; i++) {
+        const box = document.createElement('div');
+        container.appendChild(box);
+        rootSetter(num);
+        container.classList.toggle("container");
+        container.classList.toggle("container");
+        box.classList.add('box');
+        box.addEventListener('mouseover', addColor);
+
+    }
+}
+
+//createFirstGrid(16);
+createNextGrid(16);
+
+submit.addEventListener('click', function () {
+    boxRemover (container);
+    createNextGrid (gridNumberGetter ());
+});
+
+console.log(window.screen.width);
 
 
 
